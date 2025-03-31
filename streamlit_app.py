cff08207facd59e1002505e5b450fd38971a07d9
wangqisheng <wangqisheng@myhexin.com>
2025-03-31 09:40:43 +0800

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,21 @@
 import streamlit as st
 
 st.write('Hello world! I am wqs111000.')
+
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
